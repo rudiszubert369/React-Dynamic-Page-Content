eddb7e85efc2832b5b7cad2035e5e1c33bf2ce89
rudiszubert369 <szieleto@poczta.onet.pl>
2022-12-16 16:58:08 +0100

    added email validation

--- my-app/src/frontend/Newsletter.js
+++ my-app/src/frontend/Newsletter.js
@@ -1,7 +1,6 @@
 import { usePostEmail } from '../backend/usePostEmail.js';
-import React from 'react';
-import Button from './Button.js'
-
+import React, { useState } from 'react';
+import Button from './Button.js';
 
 function Newsletter() {
   const [response, error, postEmail] = usePostEmail(
@@ -9,11 +8,27 @@ function Newsletter() {
     'adchitects',
     'jsrulezzz'
   );
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  function validateEmail(email) {
+    // basic email validation regex
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address.');
+      return false;
+    }
+    setEmailError('');
+    return true;
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const { email } = event.target.elements;
-    postEmail({ email: email.value });
+    const isEmailValid = validateEmail(email);
+    if (isEmailValid) {
+      postEmail({ email: email });
+      setEmail('');
+    }
   }
 
   return (
@@ -21,18 +36,25 @@ function Newsletter() {
       <div className='newsletter'>
         <h3 className='newsletter__heading'>Sign up for our Newsletter</h3>
         <form onSubmit={handleSubmit} className='newsletter__form'>
-          <label className="visually-hidden">Type your email</label>
+          <label className='visually-hidden'>Type your email</label>
           <input
             name='email'
             type='email'
-            placeholder="Type your email"
+            placeholder='Type your email'
             className='newsletter__input'
             aria-label='Newsletter email input'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Button type='submit'>Submit</Button>
         </form>
-        {response && <p style={{ color: '#5EDC4B', fontSize:'14px', textAlign:'center' }}>{response.message}</p>}
-        {error && <p style={{ color: 'red', fontSize:'14px', textAlign:'center' }}>Error: {error.message}</p>}
+        {emailError && <p style={{ color: 'red', fontSize: '14px', textAlign: 'center' }}>{emailError}</p>}
+        {response && (
+          <p style={{ color: '#5EDC4B', fontSize: '14px', textAlign: 'center' }}>{response.message}</p>
+        )}
+        {error && (
+          <p style={{ color: 'red', fontSize: '14px', textAlign: 'center' }}>Error: {error.message}</p>
+        )}
       </div>
     </section>
   );
